d031b03708d6853ecf54d73d5901170b3159b32f
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-04-20 20:32:31 +0400

    add media header & navbar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,21 +10,21 @@ function Header() {
 	return (
 		<>
 			<div className="header">
-				<div className="header_text">
-					<h1 className="headet_text_title">Мягкие окна под ключ<span className="spanColor"> от 1 250 руб/м2</span></h1>
-				</div>
-				<div className="header_Icon">
-					<div className="header_Icon_Block">
+				<img className="header_img" src={SHukaturka} />
+				<div className="header_about">
+					<h1 className="headet_text_title">Мягкие окна под ключ <br />
+						<span className="headet_text_span"> от 1 250 руб/м2</span>
+					</h1>
+					<div className="header_about_Block about_icon">
 						<Icon className="header_Icon_Check" path={mdiCheck} />
 						<span className="header_Icon_span">Производим монтаж в течении 1 дня</span>
 					</div>
-					<div className="header_Icon_Block">
+					<div className="header_about_Block about_icon">
 						<Icon className="header_Icon_Check" path={mdiCheck} />
-						<span className="header_Icon_span">Срок производства рассчитывается индивидуально для каждого заказчика</span>
+						<span className="header_Icon_span">Срок производства рассчитывается<br /> индивидуально для каждого заказчика</span>
 					</div>
+					<Button className="header_about_Calculate">Рассчитать стоимость</Button>
 				</div>
-				<Button className="header_Calculate">Рассчитать стоимость</Button>
-				<img className="SHukaturka" src={SHukaturka} />
 			</div>
 		</>
 	)
