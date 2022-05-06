e07d1df5cef6b1ef49ddf996cac75e139de4d951
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-05-06 18:39:05 +0400

    add components

--- src/components/Soft_Box.js
+++ src/components/Soft_Box.js
@@ -1,19 +1,19 @@
 import React from "react";
-import NavBar from "./components/NavBar/NavBar";
-import Soft_Windows from "./components/SoftWindows/Soft_Windows";
-import Buy_Windows from "./components/BuyWindows/Buy_Windows";
-import Our_Advantages from "./components/OurAdvantages/Our_Advantages";
-import Cost_Windows from "./components/CostWindows/Cost_Windows";
-import Advise_You from "./components/AdviseYou/Advise_You";
-import Where_Installed from "./components/WhereInstalled/Where_Installed";
-import Window_Installation from "./components/WindowInstallation/Window_Installation";
-import What_Materials from "./components/WhatMaterials/What_Materials";
-import We_Working from "./components/WeWorking/We_Working";
-import Our_Work from "./components/OurWork/Our_Work";
-import Reviews from "./components/Reviews/Reviews";
-import O_PVC_Curtains from "./components/PVCCurtains/O_PVC_Curtains.js";
-import Discuss_Project from "./components/DiscussProject/Discuss_Project";
-import Footer from "./components/Footer/Footer";
+import NavBar from "./NavBar/NavBar";
+import Soft_Windows from "./SoftWindows/Soft_Windows";
+import Buy_Windows from "./BuyWindows/Buy_Windows";
+import Our_Advantages from "./OurAdvantages/Our_Advantages";
+import Cost_Windows from "./CostWindows/Cost_Windows";
+import Advise_You from "./AdviseYou/Advise_You";
+import Where_Installed from "./WhereInstalled/Where_Installed";
+import Window_Installation from "./WindowInstallation/Window_Installation";
+import What_Materials from "./WhatMaterials/What_Materials";
+import We_Working from "./WeWorking/We_Working";
+import Our_Work from "./OurWork/Our_Work";
+import Reviews from "./Reviews/Reviews";
+import O_PVC_Curtains from "./PVCCurtains/O_PVC_Curtains.js";
+import Discuss_Project from "./DiscussProject/Discuss_Project";
+import Footer from "./Footer/Footer";
 
 function Soft_Box() {
 	return (
