436f95bbbcce187f427bbdadffb2fea423a667d1
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-05-06 18:36:00 +0400

    new component Soft_Box

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import React from "react";
 import "./App.css";
-import NavBar from "./components/NavBar/NavBar";
-import Soft_Windows from "./components/SoftWindows/Soft_Windows";
-import Buy_Windows from "./components/BuyWindows/Buy_Windows";
-import Our_Advantages from "./components/OurAdvantages/Our_Advantages";
-import Cost_Windows from "./components/CostWindows/Cost_Windows";
-import Advise_You from "./components/AdviseYou/Advise_You";
-import Where_Installed from "./components/WhereInstalled/Where_Installed";
-import Window_Installation from "./components/WindowInstallation/Window_Installation";
-import What_Materials from "./components/WhatMaterials/What_Materials";
-import We_Working from "./components/WeWorking/We_Working";
-import Our_Work from "./components/OurWork/Our_Work";
-import Reviews from "./components/Reviews/Reviews";
-import O_PVC_Curtains from "./components/PVCCurtains/O_PVC_Curtains.js";
-import Discuss_Project from "./components/DiscussProject/Discuss_Project";
-import Footer from "./components/Footer/Footer";
+import Soft_Box from "./components/Soft_Box";
 
 function App() {
 	return (
 		<>
-			<NavBar />
-			<Soft_Windows />
-			<Buy_Windows />
-			<Our_Advantages />
-			<Cost_Windows />
-			<Advise_You />
-			<Where_Installed />
-			<Window_Installation />
-			<What_Materials />
-			<We_Working />
-			<Our_Work />
-			<Reviews />
-			<O_PVC_Curtains />
-			<Discuss_Project />
-			<Footer />
+			<Soft_Box />
 		</>
 	);
 };
---
+++ src/components/Soft_Box.js
@@ -0,0 +1,40 @@
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import Soft_Windows from "./components/SoftWindows/Soft_Windows";
+import Buy_Windows from "./components/BuyWindows/Buy_Windows";
+import Our_Advantages from "./components/OurAdvantages/Our_Advantages";
+import Cost_Windows from "./components/CostWindows/Cost_Windows";
+import Advise_You from "./components/AdviseYou/Advise_You";
+import Where_Installed from "./components/WhereInstalled/Where_Installed";
+import Window_Installation from "./components/WindowInstallation/Window_Installation";
+import What_Materials from "./components/WhatMaterials/What_Materials";
+import We_Working from "./components/WeWorking/We_Working";
+import Our_Work from "./components/OurWork/Our_Work";
+import Reviews from "./components/Reviews/Reviews";
+import O_PVC_Curtains from "./components/PVCCurtains/O_PVC_Curtains.js";
+import Discuss_Project from "./components/DiscussProject/Discuss_Project";
+import Footer from "./components/Footer/Footer";
+
+function Soft_Box() {
+	return (
+		<>
+			<NavBar />
+			<Soft_Windows />
+			<Buy_Windows />
+			<Our_Advantages />
+			<Cost_Windows />
+			<Advise_You />
+			<Where_Installed />
+			<Window_Installation />
+			<What_Materials />
+			<We_Working />
+			<Our_Work />
+			<Reviews />
+			<O_PVC_Curtains />
+			<Discuss_Project />
+			<Footer />
+		</>
+	);
+};
+
+export default Soft_Box;
